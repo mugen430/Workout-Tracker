ad9617859549156e7ead8602ed4615835c03b2d4
mugen 430 <acampado.maverick@gmail.com>
2020-09-23 21:58:32 -0500

    Updated with boiler plate and folder structure

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date
+        },
+        excercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Workout type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Workout name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Workout duration"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+)
+
+const Transaction = mongoose.model("Workouts", transactionSchema);
+
+module.exports = Transaction;
